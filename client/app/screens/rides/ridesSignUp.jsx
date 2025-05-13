772dd2f95ff12ff8686e286cf18fcfca7ddb38e4
euniceyahn <euniceyooahn@gmail.com>
2025-05-13 08:52:13 -0700

    rides signup

--- client/app/screens/rides/ridesSignUp.jsx
+++ client/app/screens/rides/ridesSignUp.jsx
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
+import { useState } from "react";
 
 const RidesSignUp = () => {
+    const [isChecked, setChecked] = useState(false);
+
     return (
+
         <View style={styles.container}>
-            <Text>Ride Info</Text>
+            <Text>Ride Signup</Text>
+            <Text>Are you a...</Text>
+            <Text>If you are a driver, how many people can you drive (excluding you?) </Text>
+            <TextInput
+                styles={styles.input}
+                placeholder="Insert Text"
+            />
+            <Text>Select which days you want a ride:</Text>
+            
+            <Text>Stay for lunch or go back immediately?</Text>
+
+            <Text>Please check this box if you are a newcomer!</Text>
+
+            <Text>Questions, comments, or concerns?</Text>
+            <TextInput
+                //styles={styles.input}
+                placeholder="Insert Text"
+            />
+
         </View>
     )
 }
