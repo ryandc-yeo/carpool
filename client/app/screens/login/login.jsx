aa47bdebf76441797b5b9e8936ca94c67fec5f23
Victoria Choi <121906670+vicchois@users.noreply.github.com>
2025-05-15 08:12:01 -0700

    login

--- client/app/screens/login/login.jsx
+++ client/app/screens/login/login.jsx
@@ -1,10 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
 import React from "react";
 
 const Login = () => {
     return (
         <View style={styles.container}>
-            <Text>Login</Text>
+            <Text style={styles.title}>Welcome!</Text>
+            <Text style={styles.title}>Enter phone number to login</Text>
+            <TextInput style={styles.input}/>
+            <Pressable onPress={() => console.log("Login pressed")} style={styles.loginButton}>
+                <Text style={styles.loginButtonText}>Login</Text>
+            </Pressable>
         </View>
     )
 }
@@ -13,8 +18,29 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'top',
-        padding: 20,
+        padding: 50,
+        marginTop: 100,
+    }, 
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    }, input: {
+        height: 40,
+        width: '100%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingLeft: 10,
+        marginBottom: 20,
+    }, loginButton: {
+        backgroundColor: 'black',
+        padding: 10,
+        borderRadius: 5,
+        width: '100%',
+        alignItems: 'center',
+    }, loginButtonText: {
+        color: 'white',
+        fontSize: 18,
     }
 });
 
